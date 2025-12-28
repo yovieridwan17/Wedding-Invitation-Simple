97ee67f58863b67c68bbc04331beb450cfcb6cbd
git <git@gmail.com>
2025-12-28 18:25:12 +0700

    edit file app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
 // App.vue sekarang bersih, tidak butuh import apa-apa
 // Kecuali kalau Bapak pakai library CSS global (misal AOS), import di sini boleh.
 </script>
-<div style="background: red; color: white; padding: 20px; text-align: center; font-weight: bold; z-index: 9999; position: relative;">
-    === INI ADALAH WEBSITE VERSI BARU ===
-  </div>
 <template>
   <router-view></router-view>
 </template>
