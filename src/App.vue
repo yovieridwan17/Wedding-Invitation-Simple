287cc5316ca3c2a33c358ce14550c369dbd26797
git <git@gmail.com>
2025-12-28 16:10:40 +0700

    edit app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
-// App.vue sekarang hanya kerangka utama
+// App.vue sekarang bersih, tidak butuh import apa-apa
+// Kecuali kalau Bapak pakai library CSS global (misal AOS), import di sini boleh.
 </script>
 
 <template>
   <router-view></router-view>
 </template>
+
+<style>
+/* Style global bisa ditaruh sini jika perlu */
+</style>
