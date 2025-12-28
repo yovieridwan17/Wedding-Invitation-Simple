19fb34f2cf8ffe8b47ed066249219d1ccfd7de04
git <git@gmail.com>
2025-12-28 16:27:45 +0700

    fix : setup main.js dan route.js

--- src/router.js
+++ src/router.js
@@ -1,33 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-// --- BAGIAN INI SAYA KEMBALIKAN SEPERTI SEMULA ---
-// Pastikan titiknya satu (.) kalau file router sejajar dengan folder components
-// Atau titiknya dua (..) kalau file router ada di dalam folder 'router'
-import Invitation from '@/components/Invitation.vue'; 
-import Dashboard from '@/components/Dashboard.vue';
-import QrScanner from '@/components/QrScanner.vue';
+// Perhatikan titiknya CUMA SATU (.) karena folder components ada di sebelahnya
+import Invitation from './components/Invitation.vue';
+import Dashboard from './components/Dashboard.vue';
+import QrScanner from './components/QrScanner.vue';
 
 const routes = [
-  { 
-    path: '/',          
-    name: 'Home', 
-    component: Invitation 
-  },
-  { 
-    path: '/admin',     
-    name: 'Admin', 
-    component: Dashboard 
-  },
-  {
-    path : '/scan',
-    name : 'scan',
-    component : QrScanner
-  }
+  { path: '/', name: 'Home', component: Invitation },
+  { path: '/admin', name: 'Admin', component: Dashboard },
+  { path: '/scan', name: 'Scan', component: QrScanner }
 ];
 
 const router = createRouter({
-  // Kita pakai Hash Mode (Pasti Aman)
-  history: createWebHashHistory(), 
+  history: createWebHashHistory(),
   routes,
 });
 
